96efe6b878b067650a9a484600a80ddeb6f210c5
RahulPawar <pawarrahul699@gmail.com>
2021-12-26 23:58:33 +0530

    add small change

---
+++ overlay-form/src/app/list-of-product/list-of-product.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListOfProductComponent } from './list-of-product.component';
+
+describe('ListOfProductComponent', () => {
+  let component: ListOfProductComponent;
+  let fixture: ComponentFixture<ListOfProductComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListOfProductComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListOfProductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ overlay-form/src/app/overlay-component/overlay-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overlay-component',
+  templateUrl: './overlay-component.component.html',
+  styleUrls: ['./overlay-component.component.scss']
+})
+export class OverlayComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
